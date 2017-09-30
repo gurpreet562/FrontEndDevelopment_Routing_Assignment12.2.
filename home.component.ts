2b2e19534d522ad48b6ef8b461cdf35adcf41936
gurpreet562 <kgurpreet562@gmail.com>
2017-09-30 20:32:58 +0530

    Create home.component.ts

---
+++ home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component({
+    template: `<h1>Welcome!</h1>
+              <p>This is Home Component </p>
+             `
+})
+ 
+export class HomeComponent {
+}
